e80481e1207d99e96b9b55b7895a7e84331b7c9a
xhquan <692278609@qq.com>
2019-04-11 01:08:55 +0800

    .

--- JSD1709_SE/src/main/java/day01/String_trim.java
+++
@@ -1,9 +0,0 @@
-package day01;
-
-public class String_trim {
-	public static void main(String[] args) {
-		String str = "    hello           ";
-		System.out.println(str.trim());
-		System.out.println(str);		
-	}
-}
