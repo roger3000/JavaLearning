6012839f462b45ef54de6e8e90ed4b28a0edf76d
Xhquan <692278609@qq.com>
2019-04-29 18:14:40 +0800

    1904291814

---
+++ WebServer_v5/readme.txt
@@ -0,0 +1,21 @@
+v2:
+将客户端发送过来的请求封装到一个HttpRequest对象中
+而不在ClientHandler中直接处理
+
+v3:
+在项目中添加webapps, 并在里面添加一个子目录 
+myweb
+然后在其中存放我们定义的页面index.html
+在ClientHandler中添加响应代码,将myweb中的index.html页面发送回客户端
+客户端测试路径 localhost:8086/index.html
+
+v4:
+将clienthandler中直接响应客户端的内容 修改成为专门定义表示
+HTTP响应的类:HttpResponse.
+重构代码
+
+v5:
+完成httprequest中对消息头的解析
+1.	在httprequest中使用map创建一个属性header
+	用于保存所有客户端发送过来的消息头
+2.	添加方法parseHeaders, 用于解析所有的消息头
---
+++ WebServer_v5/src/main/java/cn/tedu/http/HttpRequest.java
@@ -0,0 +1,119 @@
+package cn.tedu.http;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 该类的每一个实例用于表示客户端发送过来的一次HTTP请求内容
+ * 这里包含: 请求行信息 消息头 消息正文
+ * @author 许皓泉
+ *
+ */
+public class HttpRequest {
+	/*
+	 * 请求行中的内容
+	 */
+	private String method;
+	private String uri;
+	private String protocol;
+	/*
+	 * 消息头中的内容
+	 * 
+	 */
+	private Map<String, String> headers =
+			new HashMap<String, String>();
+	/**
+	 * 构造方法,通过给定的输入流读取客户端发送过来的Http请求
+	 * @param in
+	 */
+	public HttpRequest(InputStream in) {
+		/*
+		 * 解析分为三部
+		 * 1. 解析请求行
+		 * 2. 解析消息头
+		 * 3. 解析消息正文
+		 */
+		parseRequestLine(in);
+		parseHeaders(in);
+	}
+	/**
+	 * 解析请求行
+	 */
+	private void parseRequestLine(InputStream in){
+		/*
+		 * 解析请求行的大致步骤
+		 * 1. 通过输入流读取一行字符串
+		 * 	以CRLF结尾
+		 * 	CR:回车 ASC编码对应13
+		 * 	LF:换行 ASC编码对应10
+		 * 2. 将读取的请求行内容按照空格拆分
+		 * 3. 将拆出来的字符串保存到对应设置上
+		 */
+		int d = -1;
+		char c1=0,c2=0;
+		StringBuilder sb = 
+				new StringBuilder();
+		try {
+			while((d = in.read()) != -1){
+				c2 = (char)d;
+				if(c1 == 13 && c2 == 10) {
+					break;
+				}
+				sb.append((char)d);
+				c1 = c2;
+			}
+			
+			String[] str = sb.toString().trim().split("\\s");
+			
+			if(str != null && str.length > 1 ) {
+				method = str[0];
+				uri = str[1];
+				protocol = str[2];
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 解析消息头
+	 * @return
+	 */
+	private void parseHeaders(InputStream in) {
+		StringBuilder sb = 
+				new StringBuilder();
+		int d = -1;
+		char c1=0, c2=0;
+		try {
+			while((d = in.read()) != -1) {
+				System.out.println(d);
+				c2 = (char)d;
+				if(c1==13 && c2 == 10) {
+						break;
+				}else {
+					sb.append((char)d);
+				}
+				c1 = c2;
+			}
+			
+			System.out.println(sb);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String getMethod() {
+		return method;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+	
+}
