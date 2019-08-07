944981c369881c1d42c4a810192c8857ee9b289c
Xhquan <692278609@qq.com>
2019-08-07 18:28:54 +0800

    黑马javaweb

--- JSD1709_SE/src/main/java/day18/JSoupDemo01.java
+++ JSD1709_SE/src/main/java/day18/JSoupDemo01.java
@@ -1,5 +1,8 @@
 package day18;
 
+import cn.wanghaomiao.xpath.exception.XpathSyntaxErrorException;
+import cn.wanghaomiao.xpath.model.JXDocument;
+import cn.wanghaomiao.xpath.model.JXNode;
 import jdk.nashorn.internal.runtime.regexp.RegExp;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,22 +13,25 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class JSoupDemo01 {
+    ClassLoader classLoader = JSoupDemo01.class.getClassLoader();
+    String path = classLoader.getResource("student.xml").getPath();
+
     @Test
     public void JSoup(){
-        ClassLoader classLoader = JSoupDemo01.class.getClassLoader();
-        String path = classLoader.getResource("student.xml").getPath();
 
         try {
-//            Document parse = Jsoup.parse(new File(path), "utf-8");
-            URL url = new URL("http://www.bilibili.com");
-            Document dom = Jsoup.parse(url, 10000);
-            System.out.println(dom);
-            Elements elementsMatchingText = dom.getElementsMatchingText("^title\\S[\"](.*)[\"]><(.*)\"");
-            System.out.println(elementsMatchingText);
-//            Elements name = parse.getElementsByTag("name");
-//            System.out.println(name.text());
+            Document parse = Jsoup.parse(new File(path), "utf-8");
+//            URL url = new URL("http://www.bilibili.com");
+//            Document dom = Jsoup.parse(url, 10000);
+//            System.out.println(dom);
+//            Elements elementsMatchingText = dom.getElementsMatchingText("<\\S");
+//            System.out.println(elementsMatchingText);
+            Elements name = parse.getElementsByTag("name");
+            System.out.println(name.text());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -34,8 +40,6 @@ public class JSoupDemo01 {
 
     @Test
     public void Document(){
-        ClassLoader classLoader = JSoupDemo01.class.getClassLoader();
-
         try {
             Document parse = Jsoup.parse(new File(classLoader.getResource("student.xml").getPath()), "utf-8");
             Elements student = parse.getElementsByTag("student");
@@ -47,6 +51,45 @@ public class JSoupDemo01 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void selector(){
+//        使用与css相同的选择器
+        try {
+            Document parse = Jsoup.parse(new File(path), "utf-8");
+            Elements name = parse.select("name");
+//            System.out.println(name);
+            Elements student_age = parse.select("student[number=heima_0001] age");
+            System.out.println(student_age);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void selectorTest(){
+        Document document;
+        try {
+            document = Jsoup.parse(new URL("http://www.bilibili.com"), 1000);
+            Elements p = document.select("p[title]");
+            for (Element element : p) {
+                System.out.println(element.text());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void xPath() throws IOException, XpathSyntaxErrorException {
+        Document document = Jsoup.parse(new File(path), "utf-8");
+        JXDocument jxDocument = new JXDocument(document);
+        List<JXNode> jxNodes = jxDocument.selN("//student");
+        for (JXNode node : jxNodes){
+            System.out.println(node);
+        }
     }
 }
---
+++ tomcat1/src/com/hnl/servlet/ServeletDemo01.java
@@ -0,0 +1,30 @@
+package com.hnl.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ServeletDemo01 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello world");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
