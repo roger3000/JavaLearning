0c1cc9fed50e0cb4030fe217d9e2204d80c354ed
Xhquan <692278609@qq.com>
2019-08-07 10:56:39 +0800

    190807

--- JSD1709_SE/src/main/java/day04/ISR_read.java
+++ JSD1709_SE/src/main/java/day04/ISR_read.java
@@ -1,25 +1,31 @@
 package day04;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class ISR_read {
-	public static void main(String[] args) throws IOException{
-		FileInputStream fis = 
-				new FileInputStream("osw.txt");
-		
-		InputStreamReader isr =
-				new InputStreamReader(fis);
-		
-		char[] chs = new char[2];
-		
-		
-		while(isr.read(chs) != -1) {
-			System.out.print(Arrays.toString(chs));
+	public static void main(String[] args) throws IOException {
+//		FileInputStream fis =
+//				new FileInputStream("osw.txt");
+//
+//		InputStreamReader isr =
+//				new InputStreamReader(fis);
+//
+//		char[] chs = new char[2];
+//
+//
+//		while(isr.read(chs) != -1) {
+//			System.out.print(Arrays.toString(chs));
+//		}
+//
+//		isr.close();
+
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("log.txt")))) {
+
+			String i ;
+			while ((i = bufferedReader.readLine()) != null ){
+				System.out.println(i);
+			}
 		}
-		
-		isr.close();
 	}
 }
